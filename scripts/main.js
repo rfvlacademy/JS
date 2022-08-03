4f010a6f0baa15c737e89302e77c477b25e5422e
Rafael <r.falconi9030@gmail.com>
2022-08-03 12:44:01 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('#datos').on('submit', function (e) {
+        e.preventDefault();
+        let data = {};
+        data.email = $('#email').val();
+        data.name = $('#name').val();
+        data.address = $('#address').val();
+        printTable(data);
+    });
+    function printTable(data) {
+        let html = ''
+        html += '<tr>'
+        html += '<td>' + data.email + '</td>'
+        html += '<td>' + data.name + '</td>'
+        html += '<td>' + data.address + '</td>'
+        html += '<tr>'
+        $('#tbody').append(html);
+    }
+})

53e8efdd5cfbf8976d6a2bd70b28e357d8101fed
Rafael <r.falconi9030@gmail.com>
2022-08-04 20:19:12 -0500

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('#datos').on('submit', function (e) {
-        e.preventDefault();
+    $('#datos').on('submit', function (event) {
+        event.preventDefault();
         let data = {};
         data.email = $('#email').val();
         data.name = $('#name').val();
@@ -18,4 +18,18 @@ $(document).ready(function () {
         html += '<tr>'
         $('#tbody').append(html);
     }
+
+    $("input[name='color']").on('click', function () {
+        const color = $("input[name='color']:checked").val();
+        let html = `<p> el color elejido es ${color}</p>`;
+        $('#text-modal').replaceWith(html);
+        $('#myModal').modal('toggle')
+        if (color === 'blue') {
+            $("#text").addClass(color).removeClass("red white");
+        } else if (color === 'red') {
+            $("#text").addClass(color).removeClass("blue white");
+        } else {
+            $("#text").addClass(color).removeClass("red blue");
+        }
+    });
 })
